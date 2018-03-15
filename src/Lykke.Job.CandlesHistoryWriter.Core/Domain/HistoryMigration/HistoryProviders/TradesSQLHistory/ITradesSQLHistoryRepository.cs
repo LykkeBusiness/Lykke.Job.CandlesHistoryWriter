c7856dd23551bfe3f2cf7d805116fa249f440105
Dmitry Smolyakov <dmitry.smolyakov@lykke.com>
2018-03-15 18:16:42 +0300

    Initial commit: temporary/intermediate results.

--- src/Lykke.Job.CandlesHistoryWriter.Core/Domain/HistoryMigration/HistoryProviders/TradesSQLHistory/ITradesSQLHistoryRepository.cs
+++ src/Lykke.Job.CandlesHistoryWriter.Core/Domain/HistoryMigration/HistoryProviders/TradesSQLHistory/ITradesSQLHistoryRepository.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Lykke.Job.CandlesHistoryWriter.Core.Domain.HistoryMigration.HistoryProviders.TradesSQLHistory
