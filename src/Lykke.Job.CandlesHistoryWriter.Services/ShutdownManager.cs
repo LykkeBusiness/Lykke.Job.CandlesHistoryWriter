c2469afc1bd7c4704b356835bd2f2153b897a4f8
dziudan-lykke <daniel.dziubecki@lykke-business.ch>
2024-07-05 14:03:32 +0200

    LT-5517

--- src/Lykke.Job.CandlesHistoryWriter.Services/ShutdownManager.cs
+++ src/Lykke.Job.CandlesHistoryWriter.Services/ShutdownManager.cs
@@ -18,7 +18,6 @@ namespace Lykke.Job.CandlesHistoryWriter.Services
         public bool IsShuttingDown { get; private set; }
         
         private readonly ILog _log;
-        private readonly ICandlesSubscriber _candlesSubcriber;
         private readonly ISnapshotSerializer _snapshotSerializer;
         private readonly ICandlesPersistenceQueueSnapshotRepository _persistenceQueueSnapshotRepository;
         private readonly ICandlesPersistenceQueue _persistenceQueue;
@@ -28,7 +27,6 @@ namespace Lykke.Job.CandlesHistoryWriter.Services
 
         public ShutdownManager(
             ILog log,
-            ICandlesSubscriber candlesSubscriber, 
             ISnapshotSerializer snapshotSerializer,
             ICandlesPersistenceQueueSnapshotRepository persistenceQueueSnapshotRepository,
             ICandlesPersistenceQueue persistenceQueue,
@@ -39,8 +37,7 @@ namespace Lykke.Job.CandlesHistoryWriter.Services
             if (log == null)
                 throw new ArgumentNullException(nameof(log));
             _log = log.CreateComponentScope(nameof(ShutdownManager)) ?? throw new InvalidOperationException("Couldn't create a component scope for logging.");
-
-            _candlesSubcriber = candlesSubscriber ?? throw new ArgumentNullException(nameof(candlesSubscriber));
+            
             _snapshotSerializer = snapshotSerializer ?? throw new ArgumentNullException(nameof(snapshotSerializer));
             _persistenceQueueSnapshotRepository = persistenceQueueSnapshotRepository ?? throw new ArgumentNullException(nameof(persistenceQueueSnapshotRepository));
             _persistenceQueue = persistenceQueue ?? throw new ArgumentNullException(nameof(persistenceQueue));
@@ -52,14 +49,7 @@ namespace Lykke.Job.CandlesHistoryWriter.Services
         public async Task ShutdownAsync()
         {
             IsShuttingDown = true;
-
-            if (!_migrationEnabled)
-            {
-                await _log.WriteInfoAsync(nameof(ShutdownAsync), "", "Stopping candles subscriber...");
-
-                _candlesSubcriber.Stop();
-            }
-
+            
             await _log.WriteInfoAsync(nameof(ShutdownAsync), "", "Stopping persistence manager...");
                 
             _persistenceManager.Stop();
